366098d5ccd2e25c75fea3e877791e9748a98485
pr677 <pronotoshk@gmail.com>
2025-05-17 17:18:18 +0600

    Create 03_data_types.c
    generally data are three types like integer, float and char

---
+++ 03_data_types.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int a; 
+    a = 1;   // int a = 1;   is also same 
+
+    float b;
+    b = 1.4;  // float b = 1.4; 
+
+    char c;
+    c='a';    // char c = 'a'
+
+    
+    printf("The output of this program is %d",a);
+
+    return 0;
+}
