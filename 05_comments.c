cd9c92e73b8015f0a275f35849b3ba9e5603eb0f
pr677 <pronotoshk@gmail.com>
2025-05-17 17:23:06 +0600

    Create 05_comments.c
    this file show how to write single line comments and multiple line comments in C programming

---
+++ 05_comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    // I am writing this just for fun. This line doesn't do anything .
+    printf("Hello World");
+    // return 0 is returning 0 to the OS 
+    return 0;  // This is return statement 
+    /*
+    Program: 05_comments.c
+    Author: Harry 
+    Date: 3 October 2030
+    */
+}
