8d6a181bbf727d7ebda1134b15bddfa09c7e90b0
pr677 <pronotoshk@gmail.com>
2025-05-17 17:16:48 +0600

    Create 02_variable_rules.c
    this files gives examples that what a variable can be .

---
+++ 02_variable_rules.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a; 
+    int b;
+    int c;
+    int cab;
+    int harry;
+    int Harry;
+    int harry_good;           // underscore can be used at first in variables 
+    a= 7; 
+    printf("The output of this program is %d",a);
+
+    return 0;
+}
